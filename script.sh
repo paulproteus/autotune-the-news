011675504465456bf8d3bf52e3226a349d712987
Asheesh Laroia <asheesh@asheesh.org>
2014-01-01 19:17:59 -0800

    Add album

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ for thing in *.m4a
 do
 AtomicParsley "$thing" -W \
 --artist 'The Gregory Brothers' \
+--album "Auto-Tune the News" \
 --track "$(echo "$thing" | egrep -o '[0-9]+ ' | head -n1 | xargs printf "%d")"/$(ls -1 *.m4a | wc -l) \
 --title "$(echo "$thing" | egrep -o ' - (.*).m4a' | sed -r s'/\.-[a-zA-Z0-9]+\.m4a$//' | sed -r s/'^ - '//)"
 done
